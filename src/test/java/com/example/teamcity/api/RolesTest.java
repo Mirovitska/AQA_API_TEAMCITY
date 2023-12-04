5a12365540f0cf82d4594f576ef5fdff0cf40f87
Mirovitska <a.mirovitska@gmail.com>
2023-12-04 21:18:06 +0100

    HW_1

--- src/test/java/com/example/teamcity/api/RolesTest.java
+++ src/test/java/com/example/teamcity/api/RolesTest.java
@@ -16,10 +16,8 @@ public class RolesTest extends BaseApiTest {
     @Test
     public void unautorizedUserShouldNotHaveRightToCreateProject() {
 
-        //generate test data
         var testData = testDataStorage.addTestData();
 
-        //send to create project request and check
         new UncheckedProject(Specifications.getSpec().unauthSpec())
                 .create(testData.getProject())
                 .then().assertThat().statusCode(HttpStatus.SC_UNAUTHORIZED)
@@ -35,21 +33,16 @@ public class RolesTest extends BaseApiTest {
 
     @Test
     public void systemAdminShouldHaveRightsToCreateProjectTest() {
-        //generate test data
         var testData = testDataStorage.addTestData();
 
-        //set role to project
         testData.getUser().setRoles(TestDataGenerator.generateRoles(Role.SYSTEM_ADMIN, "g"));
 
-        //create user
         checkedWithSuperUser.getUserRequest().create(testData.getUser());
 
-        //login as user and create project
         var project = new CheckedProject(Specifications.getSpec()
                 .authSpec(testData.getUser()))
                 .create(testData.getProject());
 
-        //check that id created project equal to test data's project id
         softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
     }
 
@@ -81,18 +74,14 @@ public class RolesTest extends BaseApiTest {
         firstTestData.getUser().setRoles(TestDataGenerator.generateRoles(Role.PROJECT_ADMIN, "p:" +
                 firstTestData.getProject().getId()));
 
-        //create first user
         checkedWithSuperUser.getUserRequest().create(firstTestData.getUser());
 
-        //set role to second project
         secondTestData.getUser().setRoles(TestDataGenerator.generateRoles(Role.PROJECT_ADMIN, "p:" +
                 secondTestData.getProject().getId()));
-        //create second user
         checkedWithSuperUser.getUserRequest()
                 .create(secondTestData.getUser());
         System.out.println(secondTestData.getUser());
 
-        //second user try to create build config to first project
         new UncheckedBuildConfig(
                 Specifications.getSpec().authSpec(secondTestData.getUser()))
                 .create(firstTestData.getBuildType())
