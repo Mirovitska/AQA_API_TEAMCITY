e01c4705c7a9533a591222dee8f5c8c34474cb9e
Mirovitska <a.mirovitska@gmail.com>
2023-12-17 22:49:44 +0100

    second homework

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
@@ -1,9 +1,11 @@
 package com.example.teamcity.ui.pages.admin;
 
+
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.example.teamcity.ui.Selectors;
 import com.example.teamcity.ui.pages.Page;
+import lombok.Getter;
 
 import static com.codeborne.selenide.Selenide.element;
 
@@ -12,8 +14,11 @@ public class CreateNewProject extends Page {
     private SelenideElement urlInput = element(Selectors.byId("url"));
     private SelenideElement projectNameInput = element(Selectors.byId("projectName"));
     private SelenideElement buildTypeNameInput = element(Selectors.byId("buildTypeName"));
+
+    private SelenideElement error = element(Selectors.byId("errorExternalId"));
     public CreateNewProject open(String parentProjectId){
         Selenide.open("/admin/createObjectMenu.html?projectId="+parentProjectId+"&showMode=createProjectMenu");
+        waitUntilPageIsLoaded();
         return this;
     }
     public CreateNewProject createProjectByUrl(String url){
@@ -27,6 +32,10 @@ public class CreateNewProject extends Page {
         projectNameInput.sendKeys(projectName);
         buildTypeNameInput.clear();
         buildTypeNameInput.sendKeys(buildTypeName);
+        submit();
+    }
+    public void checkError() {
+        error.shouldBe();
     }
 
 
--- src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
@@ -1,20 +1,15 @@
 package com.example.teamcity.ui;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.selector.ByAttribute;
-import com.example.teamcity.api.requests.checked.CheckedUser;
+import com.codeborne.selenide.Condition;
+import com.example.teamcity.api.requests.unchecked.UncheckedProject;
 import com.example.teamcity.api.specification.Specifications;
-import com.example.teamcity.ui.pages.LoginPage;
 import com.example.teamcity.ui.pages.admin.CreateNewProject;
+import com.example.teamcity.ui.pages.favorites.ProjectsPage;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.element;
-
 public class CreateNewProjectTest extends BaseUiTest {
-
-
     @Test
-
     public void authorizedUserShouldBeAbleCreateNewProject() {
 
         var testData = testDataStorage.addTestData();
@@ -26,5 +21,29 @@ public class CreateNewProjectTest extends BaseUiTest {
                 .open(testData.getProject().getParentProject().getLocator())
                 .createProjectByUrl(url)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
+        new ProjectsPage().open()
+                .getSubprojects()
+                .stream().reduce((first, second) -> second).get()
+                .getHeader().shouldHave(Condition.text(testData.getProject().getName()));
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .get(testData.getProject().getId()).then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    //negative
+    @Test
+
+    public void authorizedUserShouldSeeErrorForInvalidProjectURL() {
+        var testData = testDataStorage.addTestData();
+        var incorrectUrl = "https://github.com/Mirovitska/Bakery123455";
+
+        loginAsUser(testData.getUser());
+        new CreateNewProject()
+                .open(testData.getProject().getParentProject().getLocator())
+                .createProjectByUrl(incorrectUrl)
+                .checkError();
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .get(testData.getProject().getId()).then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 }
